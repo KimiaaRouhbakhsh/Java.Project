4a3f2336069da6311305bd2f12039069e7a4183f
KimiaaRouhbakhsh <160735581+KimiaaRouhbakhsh@users.noreply.github.com>
2024-02-21 15:52:49 +0330

    Create a Java project
    Class definition and properties

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount {
+    public String accountNumber;
+    public String accountHolderName;
+    public double balance;
+
+    public void withdraw(double amount){
+
+    }
+    public void deposit(double amount){
+
+    }
+    public BankAccount(String accountNumber, String accountHolderName, double balance){
+        this.accountNumber =accountNumber;
+        this.accountHolderName = accountNumber;
+        this.balance = balance;
+    }
+}
